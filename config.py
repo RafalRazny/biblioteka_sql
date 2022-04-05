73162257e62d170cbf7b3fd92e741968f3eceb0b
Rafał Raźny <rafal-razny@wp.pl>
2022-04-05 13:37:36 +0200

    Apply suggested change in names and key security

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
-
+import sys
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or sys.exit("There is no correct key!")
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get('DATABASE_URL') or
-        'sqlite:///' + os.path.join(BASE_DIR, 'biblioteka.db')
+        'sqlite:///' + os.path.join(BASE_DIR, 'library.db')
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+
